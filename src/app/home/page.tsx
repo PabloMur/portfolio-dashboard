5a019012f2f5dcb80fada21125d2d95afed2e104
PabloMur <polillomurillo@gmail.com>
2023-10-16 20:26:50 -0300

    s

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,7 @@ import css from "@/styles/landing.module.css";
 export default function Home() {
   return (
     <div className={`${css.root} flex flex-grow justify-center items-center`}>
-      <h2 className="text-7xl font-bold bg-black/60 flex justify-center items-center flex-grow">
+      <h2 className="text-7xl font-bold bg-black/60 flex justify-center items-center border p-4 rounded-xl">
         WELCOME<p className="text-blue-500"> Pablo</p>
       </h2>
     </div>
